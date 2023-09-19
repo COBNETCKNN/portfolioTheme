dff4c26c07d75d4f30977ac3a6853ecc503a8099
Ahmed Meshkin <ahmedmeshkin@gmail.com>
2023-09-19 16:21:05 +0200

    laravel mix installed

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php 
+
+function portfolioTheme_files() {
+    //enqueue CSS
+    wp_enqueue_style('mainCSS', get_template_directory_uri() . '/css/main.css');
+
+    //enqueue JS
+   wp_enqueue_script('mainJS', get_stylesheet_directory_uri() . '/js/main.js', array(), 1.0, true);
+}
+
+?>
